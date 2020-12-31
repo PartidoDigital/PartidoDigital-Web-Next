290abef753617274e539f5d9d84e20c8334b82a7
Leo Giovanetti <hello@leog.me>
2020-12-30 21:18:59 -0300

    Sacando css imports

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ const jekyllEnv = process.env.JEKYLL_ENV || process.env.NODE_ENV || "development
 
 module.exports = {
   plugins: [
-    require("postcss-import")({ path: "./assets/style" }),
     require("tailwindcss"),
     require("autoprefixer"),
     ...(jekyllEnv !== "development" ? [
