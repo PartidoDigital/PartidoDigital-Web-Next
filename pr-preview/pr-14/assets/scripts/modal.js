ec2b0cae0773a0b719a6038273c2eec6376f9451
leog <leog@users.noreply.github.com>
2022-11-20 23:11:17 +0000

    deploy: f3d3b35d610972e1695a4ad930435f8d4cb6f225

--- pr-preview/pr-14/assets/scripts/modal.js
+++
@@ -1,5 +0,0 @@
-jQuery(function () {
-  window.videoModal = (id) => {
-    return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-  }
-});
